b8add7d145ae13761b75fb82b1f5ed92654e9c9c
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-01-30 16:41:11 +0530

    2nd

--- src/main/java/org/example/JDBC.java
+++ src/main/java/org/example/JDBC.java
@@ -1,24 +1,43 @@
 package org.example;
 
 import java.sql.*;
-
+import java.util.*;
 class DC {
 
 
     public static void main(String args[]) throws SQLException {
         DC n = DC.getInstance();
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "Srini@067");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("Select * from student");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
-                System.out.println("DB Connection Working");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String user = "root";
+        System.out.println("Enter the password");
+        Scanner sc = new Scanner(System.in);
+        String pass = sc.next();
+        Connection connection = DriverManager.getConnection(url, user, pass);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select * from student");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+            System.out.println("DB Connection Working");
+        }
+        /*} catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+        int ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                n.connect(url, user, pass);
+                break;
+            case 2:
+                n.closeconnection();
+                break;
+
+            default:
+
+                System.exit(0);
+        }
     }
         private static DC instance=null;
         private Connection connection;
@@ -33,7 +52,9 @@ public void connect(String url,String user,String pass){
         e.printStackTrace();
     }
 }
-
+        public void closeconnection(){
+            closeconnection();
+        }
         public static DC getInstance() throws SQLException {
             if (instance == null) {
                 instance = new DC();
